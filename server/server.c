6619072c4988380c37cb36f881f4930412705faa
Sebastiano Marino <seby.marino.sm@gmail.com>
2025-06-13 10:24:28 +0200

    prime modifiche

---
+++ server/server.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "server.h"
+
+int main(){
+
+    int server_fd = inizializzazione_server();
+    pritnf("server FTP in ascolto sulla porta : %d\n", PORT);
+
+    attesa_connessioni(server_fd);
+
+    close(server_fd);
+    printf("chiusura del server");
+
+    return 0;
+}
---
+++ server/server.h
@@ -0,0 +1,15 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <netinet/in.h>
+#include <pthread.h>
+
+#define PORT 2121 //Porta di ascolto server FTP
+
+int inizializzazione_server();//funzione principale server
+
+void attesa_connessioni(int server_fd);
+
+void *gestione_client(void *arg); //funzione eseguita da un thread per gestire la comunicazione con un client
+
+#endif SERVER_H
