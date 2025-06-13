507e4bf5ae3221d6818e031d620a2befe972bffe
Sebastiano Marino <seby.marino.sm@gmail.com>
2025-06-13 12:34:37 +0200

    modifiche

--- server/comandi.h
+++ server/comandi.h
@@ -1,6 +1,14 @@
 #ifndef COMANDI_H
 #define COMANDI_H
+#include <limits.h> // per PATH_MAX (inerente al massimo percorso di cartelle)
+#define FTP_ROOT "./ftp_root"
 
-void gestisci_comando(int client_fd, const char *comando);
+typedef struct {
+    int client_fd;
+    char directory_corrente[PATH_MAX];
+}Sessione;
+
+
+void gestisci_comando(Sessione *sessione, const char *comando);
 
 #endif COMANDI_H
